75fe9faa88726e969b8e18d2ea7460c86344ab5d
Gabriel Charleboix <gcharleboix@gmail.com>
2025-05-30 19:59:01 -0500

    Update headless triangle example for wgpu-native v24.0.3.1

--- examples/triangle/triangle.zig
+++ examples/triangle/triangle.zig
@@ -10,8 +10,10 @@ const output_extent = wgpu.Extent3D {
 const output_bytes_per_row = 4 * output_extent.width;
 const output_size = output_bytes_per_row * output_extent.height;
 
-fn handle_buffer_map(status: wgpu.BufferMapAsyncStatus, _: ?*anyopaque) callconv(.C) void {
+fn handleBufferMap(status: wgpu.MapAsyncStatus, _: wgpu.StringView, userdata1: ?*anyopaque, _: ?*anyopaque) callconv(.C) void {
     std.log.info("buffer_map status={x:.8}\n", .{@intFromEnum(status)});
+    const complete: *bool = @ptrCast(@alignCast(userdata1));
+    complete.* = true;
 }
 
 // Based off of headless triangle example from https://github.com/eliemichel/LearnWebGPU-Code/tree/step030-headless
@@ -20,16 +22,16 @@ pub fn main() !void {
     const instance = wgpu.Instance.create(null).?;
     defer instance.release();
 
-    const adapter_request = instance.requestAdapterSync(&wgpu.RequestAdapterOptions {});
+    const adapter_request = instance.requestAdapterSync(&wgpu.RequestAdapterOptions {}, 0);
     const adapter = switch(adapter_request.status) {
         .success => adapter_request.adapter.?,
         else => return error.NoAdapter,
     };
     defer adapter.release();
 
-    const device_request = adapter.requestDeviceSync(&wgpu.DeviceDescriptor {
+    const device_request = adapter.requestDeviceSync(instance, &wgpu.DeviceDescriptor {
         .required_limits = null,
-    });
+    }, 0);
     const device = switch(device_request.status) {
         .success => device_request.device.?,
         else => return error.NoDevice,
@@ -42,15 +44,15 @@ pub fn main() !void {
     const swap_chain_format = wgpu.TextureFormat.bgra8_unorm_srgb;
 
     const target_texture = device.createTexture(&wgpu.TextureDescriptor {
-        .label = "Render texture",
+        .label = wgpu.StringView.fromSlice("Render texture"),
         .size = output_extent,
         .format = swap_chain_format,
-        .usage = wgpu.TextureUsage.render_attachment | wgpu.TextureUsage.copy_src,
+        .usage = wgpu.TextureUsages.render_attachment | wgpu.TextureUsages.copy_src,
     }).?;
     defer target_texture.release();
 
     const target_texture_view = target_texture.createView(&wgpu.TextureViewDescriptor {
-        .label = "Render texture view",
+        .label = wgpu.StringView.fromSlice("Render texture view"),
         .mip_level_count = 1,
         .array_layer_count = 1,
     }).?;
@@ -61,8 +63,8 @@ pub fn main() !void {
     defer shader_module.release();
 
     const staging_buffer = device.createBuffer(&wgpu.BufferDescriptor {
-        .label = "staging_buffer",
-        .usage = wgpu.BufferUsage.map_read | wgpu.BufferUsage.copy_dst,
+        .label = wgpu.StringView.fromSlice("staging_buffer"),
+        .usage = wgpu.BufferUsages.map_read | wgpu.BufferUsages.copy_dst,
         .size = output_size,
         .mapped_at_creation = @as(u32, @intFromBool(false)),
     }).?;
@@ -89,12 +91,12 @@ pub fn main() !void {
     const pipeline = device.createRenderPipeline(&wgpu.RenderPipelineDescriptor {
         .vertex = wgpu.VertexState {
             .module = shader_module,
-            .entry_point = "vs_main",
+            .entry_point = wgpu.StringView.fromSlice("vs_main"),
         },
         .primitive = wgpu.PrimitiveState {},
         .fragment = &wgpu.FragmentState {
             .module = shader_module,
-            .entry_point = "fs_main",
+            .entry_point = wgpu.StringView.fromSlice("fs_main"),
             .target_count = color_targets.len,
             .targets = color_targets.ptr
         },
@@ -106,7 +108,7 @@ pub fn main() !void {
         const next_texture = target_texture_view;
 
         const encoder = device.createCommandEncoder(&wgpu.CommandEncoderDescriptor {
-            .label = "Command Encoder",
+            .label = wgpu.StringView.fromSlice("Command Encoder"),
         }).?;
         defer encoder.release();
 
@@ -131,12 +133,12 @@ pub fn main() !void {
 
         defer next_texture.release();
 
-        const img_copy_src = wgpu.ImageCopyTexture {
+        const img_copy_src = wgpu.TexelCopyTextureInfo {
             .origin = wgpu.Origin3D {},
             .texture = target_texture,
         };
-        const img_copy_dst = wgpu.ImageCopyBuffer {
-            .layout = wgpu.TextureDataLayout {
+        const img_copy_dst = wgpu.TexelCopyBufferInfo {
+            .layout = wgpu.TexelCopyBufferLayout {
                 .bytes_per_row = output_bytes_per_row,
                 .rows_per_image = output_extent.height,
             },
@@ -146,14 +148,22 @@ pub fn main() !void {
         encoder.copyTextureToBuffer(&img_copy_src, &img_copy_dst, &output_extent);
 
         const command_buffer = encoder.finish(&wgpu.CommandBufferDescriptor {
-            .label = "Command Buffer",
+            .label = wgpu.StringView.fromSlice("Command Buffer"),
         }).?;
         defer command_buffer.release();
 
         queue.submit(&[_]*const wgpu.CommandBuffer{command_buffer});
 
-        staging_buffer.mapAsync(wgpu.MapMode.read, 0, output_size, handle_buffer_map, null);
-        _ = device.poll(true, null);
+        var buffer_map_complete = false;
+        _ = staging_buffer.mapAsync(wgpu.MapModes.read, 0, output_size, wgpu.BufferMapCallbackInfo {
+            .callback = handleBufferMap,
+            .userdata1 = @ptrCast(&buffer_map_complete),
+        });
+        instance.processEvents();
+        while(!buffer_map_complete) {
+            instance.processEvents();
+        }
+        // _ = device.poll(true, null);
 
         const buf: [*]u8 = @ptrCast(@alignCast(staging_buffer.getMappedRange(0, output_size).?));
         defer staging_buffer.unmap();
